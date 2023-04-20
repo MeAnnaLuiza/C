f6525c90e218c4205e7113ba98e4deb487ecf1dd
Anna Luiza <anna.luiza.9919@gmail.com>
2023-04-19 22:44:08 -0300

    Fundamentos

--- Fundamentos/ex29.c
+++ Fundamentos/ex29.c
@@ -3,6 +3,24 @@ imprima todos os números inteiros pares no intervalo aberto e o somatório dess
 números. Suponha que os números digitados sejam crescentes, ou seja, o primeiro
 valor é menor que o segundo.*/
 
+#include <stdio.h>
+
 int main(void){
-    return;
+    int min, max, soma = 0;
+
+    printf("digite o limite inferior: ");
+    scanf("%d", &min);
+
+    printf("digite o limite superior: ");
+    scanf("%d", &max);
+
+    for(int i = min + 1; i < max; i++){
+        if(i % 2 == 0){
+            soma += i;
+        }
+    }
+
+    printf("soma dos números pares no intervalo aberto e: %d\n", soma);
+
+    return 0;
 }
