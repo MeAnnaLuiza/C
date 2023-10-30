48718307b10f7e13b2201f33b478b654a3c031db
Anna Luiza <anna.luiza.9919@gmail.com>
2023-10-29 23:58:37 -0300

    ajuste

--- Estruturas de dados/TAD Ponto/main.c
+++ Estruturas de dados/TAD Ponto/main.c
@@ -4,10 +4,8 @@
 int main(){
     float d;
     Ponto *p,*q;
-    //Ponto r; //ERRO
     p = pto_cria(10,21);
     q = pto_cria(7,25);
-    //q->x = 2; //ERRO
     d = pto_distancia(p,q);
     printf("Distancia entre pontos: %f\n",d);
 
