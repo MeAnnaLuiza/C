b397fd948405994f25b9ee04d072491cb6e32e33
MeAnnaLuiza <anna.luiza.9919@gmail.com>
2023-12-11 23:21:46 -0300

    Ajuste

--- Estruturas de dados/Fila Dinamica/FilaDin.c
+++ Estruturas de dados/Fila Dinamica/FilaDin.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "FilaDin.h" //inclui os Protótipos
-//Definição do tipo Fila
+#include "FilaDin.h" //inclui os Protï¿½tipos
 struct elemento{
     struct aluno dados;
     struct elemento *prox;
 };
 typedef struct elemento Elem;
-//Definição do Nó Descritor da Fila
 struct fila{
     struct elemento *inicio;
     struct elemento *final;
