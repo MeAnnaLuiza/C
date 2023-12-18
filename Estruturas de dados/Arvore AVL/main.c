1cbc7b33bdb354d9e1aeaa0857891a04154ec87c
MeAnnaLuiza <anna.luiza.9919@gmail.com>
2023-12-17 23:07:03 -0300

    Ajuste

--- Estruturas de dados/Arvore AVL/main.c
+++ Estruturas de dados/Arvore AVL/main.c
@@ -5,7 +5,6 @@
 int main(){
     ArvAVL* avl;
     int res,i;
-    //int N = 11, dados[11] = {50,25,10,5,5,7,3,30,20,8,15};
     int N = 10, dados[10] = {1,2,3,10,4,5,9,7,8,6};
 
     avl = cria_ArvAVL();
@@ -19,7 +18,7 @@ int main(){
 
     printf("\nAVL tree:\n");
     emOrdem_ArvAVL(avl);
-    printf("\n\n")
+    printf("\n\n");
 
     remove_ArvAVL(avl,6);
     printf("\nAVL tree:\n");
