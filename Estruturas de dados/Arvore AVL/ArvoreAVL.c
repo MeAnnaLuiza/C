a0585ec7778a12fe382febb38996bec2889fc30b
MeAnnaLuiza <anna.luiza.9919@gmail.com>
2023-12-17 23:03:41 -0300

    Ajuste

--- Estruturas de dados/Arvore AVL/ArvoreAVL.c
+++ Estruturas de dados/Arvore AVL/ArvoreAVL.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "ArvoreAVL.h" //inclui os Protótipos
+#include "ArvoreAVL.h" //inclui os Protï¿½tipos
 
 struct NO{
     int info;
@@ -28,7 +28,7 @@ void libera_NO(struct NO* no){
 void libera_ArvAVL(ArvAVL* raiz){
     if(raiz == NULL)
         return;
-    libera_NO(*raiz);//libera cada nó
+    libera_NO(*raiz);//libera cada nï¿½
     free(raiz);//libera a raiz
 }
 
@@ -130,8 +130,7 @@ int consulta_ArvAVL(ArvAVL *raiz, int valor){
     return 0;
 }
 
-//=================================
-void RotacaoLL(ArvAVL *A){//LL
+void RotacaoLL(ArvAVL *A){
     printf("RotacaoLL\n");
     struct NO *B;
     B = (*A)->esq;
@@ -142,7 +141,7 @@ void RotacaoLL(ArvAVL *A){//LL
     *A = B;
 }
 
-void RotacaoRR(ArvAVL *A){//RR
+void RotacaoRR(ArvAVL *A){
     printf("RotacaoRR\n");
     struct NO *B;
     B = (*A)->dir;
@@ -153,19 +152,19 @@ void RotacaoRR(ArvAVL *A){//RR
     (*A) = B;
 }
 
-void RotacaoLR(ArvAVL *A){//LR
+void RotacaoLR(ArvAVL *A){
     RotacaoRR(&(*A)->esq);
     RotacaoLL(A);
 }
 
-void RotacaoRL(ArvAVL *A){//RL
+void RotacaoRL(ArvAVL *A){
     RotacaoLL(&(*A)->dir);
     RotacaoRR(A);
 }
 
 int insere_ArvAVL(ArvAVL *raiz, int valor){
     int res;
-    if(*raiz == NULL){//árvore vazia ou nó folha
+    if(*raiz == NULL){//ï¿½rvore vazia ou nï¿½ folha
         struct NO *novo;
         novo = (struct NO*)malloc(sizeof(struct NO));
         if(novo == NULL)
@@ -223,8 +222,8 @@ struct NO* procuraMenor(struct NO* atual){
 }
 
 int remove_ArvAVL(ArvAVL *raiz, int valor){
-	if(*raiz == NULL){// valor não existe
-	    printf("valor não existe!!\n");
+	if(*raiz == NULL){// valor nï¿½o existe
+	    printf("valor nï¿½o existe!!\n");
 	    return 0;
 	}
 
@@ -252,14 +251,14 @@ int remove_ArvAVL(ArvAVL *raiz, int valor){
 	}
 
 	if((*raiz)->info == valor){
-	    if(((*raiz)->esq == NULL || (*raiz)->dir == NULL)){// nó tem 1 filho ou nenhum
+	    if(((*raiz)->esq == NULL || (*raiz)->dir == NULL)){// nï¿½ tem 1 filho ou nenhum
 			struct NO *oldNode = (*raiz);
 			if((*raiz)->esq != NULL)
                 *raiz = (*raiz)->esq;
             else
                 *raiz = (*raiz)->dir;
 			free(oldNode);
-		}else { // nó tem 2 filhos
+		}else { // nï¿½ tem 2 filhos
 			struct NO* temp = procuraMenor((*raiz)->dir);
 			(*raiz)->info = temp->info;
 			remove_ArvAVL(&(*raiz)->dir, (*raiz)->info);
